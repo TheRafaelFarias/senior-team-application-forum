8e0e5e5896b228674ddbe6a064332102c8e76d10
Rafael Farias <rafaelfarias@zohomail.com>
2023-01-23 01:29:48 -0300

    feat(components): add a Div component to create basic flex divs easier

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle<{
   fontFamilySrc: string;
@@ -22,4 +22,21 @@ export const GlobalStyle = createGlobalStyle<{
     color: inherit;
     text-decoration: none;
   }
+`;
+
+export const Div = styled.div<{
+  gapY?: number;
+  gapX?: number;
+  alignItems?: "center";
+  justifyContent?: "center" | "space-between" | "space-around";
+  flexDirection: "row" | "column";
+}>`
+  display: flex;
+  flex-direction: ${(props) => props.flexDirection};
+
+  ${(props) => props.gapY && `row-gap: ${props.gapY}rem`}
+  ${(props) => props.gapX && `column-gap: ${props.gapX}rem`}
+  ${(props) => props.alignItems && `align-items: ${props.alignItems}`}
+  ${(props) =>
+    props.justifyContent && `justify-content: ${props.justifyContent}`}
 `;
