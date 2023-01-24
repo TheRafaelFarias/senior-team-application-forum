60dd3fe55daaca0080272a278161765eeb40be12
Rafael Farias <rafaelfarias@zohomail.com>
2023-01-24 00:00:38 -0300

    feat(components): create dropdown component

---
+++ src/components/dropdown/index.tsx
@@ -0,0 +1,64 @@
+import { defaultTheme } from "@/config/theme";
+import { useState } from "react";
+import { FaChevronDown } from "react-icons/fa";
+import {
+  DropdownContainer,
+  DropdownDisplayOptions,
+  DropdownOption,
+  DropdownOptions,
+  DropdownPlaceholder,
+  DropdownSelectedOption,
+} from "./styles";
+
+interface DropdownProps<T extends string> {
+  data: Array<T>;
+  onClickOption?: (value: T) => void;
+}
+
+const Dropdown = <T extends string>({
+  data,
+  onClickOption,
+}: DropdownProps<T>) => {
+  const [selectedOption, setSelectedOption] = useState<T | undefined>(
+    undefined
+  );
+  const [isDropdownOpen, setIsDropdownOpen] = useState(true);
+
+  return (
+    <DropdownContainer>
+      <DropdownPlaceholder>Select a category</DropdownPlaceholder>
+      <DropdownOptions>
+        <DropdownSelectedOption
+          isDropdownOpen={isDropdownOpen}
+          hasSelectedValue={Boolean(selectedOption)}
+          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+        >
+          {selectedOption ?? "Click here to see all categories"}
+          <FaChevronDown color={defaultTheme.quartiaryText} />
+        </DropdownSelectedOption>
+        {isDropdownOpen && (
+          <DropdownDisplayOptions>
+            {data?.length >= 1 &&
+              data.map(
+                (value, index) =>
+                  selectedOption !== value && (
+                    <DropdownOption
+                      key={index}
+                      onClick={() => {
+                        setSelectedOption(value);
+                        onClickOption && onClickOption(value);
+                        setIsDropdownOpen(!isDropdownOpen);
+                      }}
+                    >
+                      {value}
+                    </DropdownOption>
+                  )
+              )}
+          </DropdownDisplayOptions>
+        )}
+      </DropdownOptions>
+    </DropdownContainer>
+  );
+};
+
+export default Dropdown;
---
+++ src/components/dropdown/styles.ts
@@ -0,0 +1,96 @@
+import { convertHexToRGB } from "@/helpers/colors";
+import styled, { css } from "styled-components";
+
+export const DropdownContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 1rem;
+`;
+
+export const DropdownPlaceholder = styled.p`
+  color: ${(props) => props.theme.quartiaryText};
+  font-size: 0.8rem;
+  font-weight: 400;
+`;
+
+export const DropdownOptions = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`;
+
+export const DropdownDisplayOptions = styled.div`
+  position: absolute;
+  top: 100%;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-height: calc(37px * 4 + 18px);
+  overflow-y: auto;
+  border-bottom-left-radius: 0.375rem;
+  border-bottom-right-radius: 0.375rem;
+
+  &::-webkit-scrollbar {
+    width: 5px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: ${(props) => props.theme.secondary};
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 999px;
+    background-color: ${(props) => props.theme.beige};
+  }
+`;
+
+export const DropdownOption = styled.button`
+  border: none;
+  text-align: start;
+  color: ${(props) => props.theme.secondaryText};
+  background: ${(props) => props.theme.secondary};
+  padding: 0.625rem 1.25rem;
+  transition: 200ms;
+
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${(props) =>
+      convertHexToRGB(props.theme.quartiaryText, 0.2)};
+  }
+`;
+
+export const DropdownSelectedOption = styled(DropdownOption)<{
+  hasSelectedValue: boolean;
+  isDropdownOpen: boolean;
+}>`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 0.375rem;
+  transition: all 200ms;
+
+  &:hover {
+    background: ${(props) => props.theme.secondary};
+  }
+
+  & > svg {
+    transition: all 200ms;
+    transform: rotate(0deg);
+  }
+
+  ${(props) => props.hasSelectedValue && `color: ${props.theme.primaryText};`}
+  ${(props) =>
+    props.isDropdownOpen &&
+    css`
+      border-bottom-left-radius: 0px;
+      border-bottom-right-radius: 0px;
+
+      & > svg {
+        transform: rotate(180deg);
+      }
+    `}
+`;
