95ec0b5daeefbc886aabfd2363736f3996ab7731
Rafael Farias <rafaelfarias@zohomail.com>
2023-01-26 02:02:17 -0300

    feat(components): logo on navbar redirect to home page and add a button to open create a new thread modal

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-import { BiCaretDown } from "react-icons/bi";
-import { FiLogIn } from "react-icons/fi";
-import { HiMagnifyingGlass } from "react-icons/hi2";
+import { IoMdCreate } from "react-icons/io";
+import { useModal } from "../modal/hooks/useModal";
 import UserImage from "../userImage";
 import {
   NavbarButton,
@@ -14,35 +14,34 @@ import {
 } from "./styles";
 
 const Navbar: React.FC = () => {
-  const isLogged = true;
+  const { changeCurrentModal } = useModal();
 
   return (
     <NavbarContainer>
       <NavbarSideContent>
-        <Image
-          src="/logo.png"
-          width={40}
-          height={40}
-          alt="Junior Team Forums logo that has a gradient orange background with a white lighting in the center of it"
-        />
-        <NavbarWebsiteText>Junior Team Forums</NavbarWebsiteText>
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            width={40}
+            height={40}
+            alt="Junior Team Forums logo that has a gradient orange background with a white lighting in the center of it"
+          />
+          <NavbarWebsiteText>Junior Team Forums</NavbarWebsiteText>
+        </Link>
       </NavbarSideContent>
 
       <NavbarSideContent>
-        <NavbarButton>
-          <HiMagnifyingGlass size={20} />
+        <NavbarButton
+          onClick={() => {
+            changeCurrentModal("createNewThread");
+          }}
+        >
+          <IoMdCreate size={25} />
         </NavbarButton>
-        {isLogged ? (
-          <NavbarUserInformationsContainer>
-            <UserImage />
-            <NavbarUsername>Rafael Farias</NavbarUsername>
-            <BiCaretDown color="white" size={16} />
-          </NavbarUserInformationsContainer>
-        ) : (
-          <NavbarButton>
-            <FiLogIn color="white" size={18} />
-          </NavbarButton>
-        )}
+        <NavbarUserInformationsContainer>
+          <UserImage />
+          <NavbarUsername>Rafael Farias</NavbarUsername>
+        </NavbarUserInformationsContainer>
       </NavbarSideContent>
     </NavbarContainer>
   );
