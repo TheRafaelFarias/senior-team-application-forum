ca528cc707e838501afdcc368162057e4f92e056
Rafael Farias <rafaelfarias@zohomail.com>
2023-02-04 21:56:50 -0300

    fix(integration): add missing thread with user types

--- src/types/thread.ts
+++ src/types/thread.ts
@@ -1,18 +1,27 @@
+import { User } from "firebase/auth";
 import { Category } from "./category";
-import { ThreadComment } from "./comment";
+import { ThreadComment, ThreadCommentWithAuthorObject } from "./comment";
 import { TopicWithoutThread } from "./topic";
 
 export interface Thread {
   id: string;
   title: string;
-  createdAt: Date;
+  createdAt: number;
   content: string;
-  comments: Array<ThreadComment>;
+  comments: Array<ThreadComment | ThreadCommentWithAuthorObject>;
+  authorId: string;
 }
 
 export type ThreadWithCategoryAndTopicNames = {
   topic: TopicWithoutThread;
   category: Pick<Category, "id" | "name">;
-} & Thread;
+} & ThreadWithUser;
 
 export type ThreadPreview = Omit<Thread, "content" | "comments">;
+export type ThreadPreviewWithAuthor = ThreadPreview & {
+  author: User;
+};
+
+export type ThreadWithUser = Thread & {
+  author: User;
+};
