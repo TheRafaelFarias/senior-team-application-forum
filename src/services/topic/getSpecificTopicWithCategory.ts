0035b2c422629795284f5818f513533e8c341442
Rafael Farias <rafaelfarias@zohomail.com>
2023-02-04 22:09:32 -0300

    fix(integration): getSpecificTopicWithCategory pass thread preview author information

--- src/services/topic/getSpecificTopicWithCategory.ts
+++ src/services/topic/getSpecificTopicWithCategory.ts
@@ -1,5 +1,5 @@
 import { Category } from "@/types/category";
-import { ThreadPreview } from "@/types/thread";
+import { ThreadPreview, ThreadPreviewWithAuthor } from "@/types/thread";
 import { Topic } from "@/types/topic";
 import {
   collection,
@@ -20,19 +20,34 @@ export async function getSpecificTopicWithCategoryAndAllThreadsPreview(
 
   const topicRef = doc(firestore, "categories", categoryId, "topics", topicId);
   const topicDoc = await getDoc(topicRef);
-  const topic = { ...topicDoc.data(), id: topicDoc.id } as Topic<ThreadPreview>;
+  const topic = {
+    ...topicDoc.data(),
+    id: topicDoc.id,
+  } as Topic<ThreadPreviewWithAuthor>;
 
   const threadsQuery = query(
     collection(firestore, `categories/${categoryId}/topics/${topicId}/threads`)
   );
   const threadsDocument = await getDocs(threadsQuery);
-  const threads = threadsDocument.docs.map((document) => {
-    const topic = document.data();
-    return {
-      id: document.id,
-      ...topic,
-    };
-  }) as Array<ThreadPreview>;
+  const threads = (await Promise.all(
+    threadsDocument.docs.map(async (document) => {
+      const threadPreview = document.data() as Omit<ThreadPreview, "id">;
+
+      const threadPreviewAuthorRef = doc(
+        firestore,
+        "users",
+        threadPreview.authorId
+      );
+      const threadPreviewAuthorDoc = await getDoc(threadPreviewAuthorRef);
+      const threadPreviewAuthor = threadPreviewAuthorDoc.data();
+
+      return {
+        id: document.id,
+        author: threadPreviewAuthor,
+        ...threadPreview,
+      };
+    })
+  )) as Array<ThreadPreviewWithAuthor>;
 
   topic["threads"] = threads;
 
